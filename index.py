7db4978388c4d296ffdc16c8236c07bbaec24c68
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-11-10 02:46:45 -0500

    Update index.py

--- index.py
+++ index.py
@@ -4,53 +4,65 @@ from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0
 import dash_bootstrap_components as dbc
 
 
-# get weblinks for scraper
-def scrape_weblinks():
-    from datetime import date
+# # get weblinks for scraper
+# def scrape_weblinks():
+#     from datetime import date
 
-    start_date = '04/28/2013'
+#     start_date = '04/28/2013'
 
-    def allsundays(start):
-        return pd.date_range(start=start, end=date.today(),
-                             freq='W-SUN').strftime('%Y/%m/%d').tolist()
+#     def allsundays(start):
+#         return pd.date_range(start=start, end=date.today(),
+#                              freq='W-SUN').strftime('%Y/%m/%d').tolist()
 
-    date_list = allsundays(start_date)
+#     date_list = allsundays(start_date)
 
-    for i in date_list:
-        print('https://coinmarketcap.com/historical/' + i.replace('/', '') + '/')
-
-
-# scrape_weblinks()
-
-external_stylesheets = [dbc.themes.CERULEAN]
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-# df = pd.read_csv('coin_rankings.csv')
-df = pd.read_csv('coin_rankings_fulldata.csv', parse_dates=['Date'])
-name_dict = pd.read_csv('ticker_name.csv')
-
-df = pd.merge(df, name_dict, how="left", on='label')
-
-# print(df); quit()
+#     for i in date_list:
+#         print('https://coinmarketcap.com/historical/' + i.replace('/', '') + '/')
 
 
+# # scrape_weblinks()
 
-stablecoins = ['USDTTether', 'USDCUSD Coin', 'BUSDBinance USD']
+# external_stylesheets = [dbc.themes.CERULEAN]
+# app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-df_rank = df.groupby(['Rank', 'Name']).size().reset_index().rename(columns={0: '# of Weeks'}).drop_duplicates(
-    subset=['Name'], keep='first').sort_values(by=['Rank', '# of Weeks'], ascending=[True, False])
+# # df = pd.read_csv('coin_rankings.csv')
+# df = pd.read_csv('coin_rankings_fulldata.csv', parse_dates=['Date'])
+# name_dict = pd.read_csv('ticker_name.csv')
+
+# df = pd.merge(df, name_dict, how="left", on='label')
+
+# # print(df); quit()
 
 
-last_date = str(max(df.Date))[:10]
 
-# print(df_rank); quit()
+# stablecoins = ['USDTTether', 'USDCUSD Coin', 'BUSDBinance USD']
 
-updated = html.Div(
-    [
-        html.P("Last updated: " + last_date,
-                style={'text-align': 'right', 'color': 'light grey'})
-    ], className='card-title'
-)
+# df_rank = df.groupby(['Rank', 'Name']).size().reset_index().rename(columns={0: '# of Weeks'}).drop_duplicates(
+#     subset=['Name'], keep='first').sort_values(by=['Rank', '# of Weeks'], ascending=[True, False])
+
+
+# last_date = str(max(df.Date))[:10]
+
+# # print(df_rank); quit()
+
+# updated = html.Div(
+#     [
+#         html.P("Last updated: " + last_date,
+#                 style={'text-align': 'right', 'color': 'light grey'})
+#     ], className='card-title'
+# )
+
+
+
+# table = dbc.Table.from_dataframe(df_rank[['Rank', 'Name', '# of Weeks']], striped=True, bordered=True, hover=True)
+
+# attribution = dbc.Row(
+#     [dbc.Row([
+#         dbc.CardLink("Created by: @tc_madt", href="https://www.twitter.com/tc_madt")],
+#     ),
+#         dbc.Row([dbc.CardLink("Data Source: CoinMarketCap", href="https://coinmarketcap.com/")]),
+#     ]
+# )
 
 title = html.Div(
     [
@@ -61,20 +73,12 @@ title = html.Div(
     ], className='card-title'
 )
 
-table = dbc.Table.from_dataframe(df_rank[['Rank', 'Name', '# of Weeks']], striped=True, bordered=True, hover=True)
-
-attribution = dbc.Row(
-    [dbc.Row([
-        dbc.CardLink("Created by: @tc_madt", href="https://www.twitter.com/tc_madt")],
-    ),
-        dbc.Row([dbc.CardLink("Data Source: CoinMarketCap", href="https://coinmarketcap.com/")]),
-    ]
-)
-
-app.layout = html.Div([updated,
+app.layout = html.Div([
+#     updated,
                        title,
-                       table,
-                       attribution]
+#                        table,
+#                        attribution
+]
                       ,
                       style={'margin-top': 10, 'margin-bottom': 10,
                              'margin-right': '25px', 'margin-left': '25px',
