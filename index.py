e67ad270db258d877a5397600ea2e749653b814f
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-11-10 03:03:28 -0500

    Update index.py

--- index.py
+++ index.py
@@ -25,8 +25,8 @@ import dash_bootstrap_components as dbc
 
 # # scrape_weblinks()
 
-# external_stylesheets = [dbc.themes.CERULEAN]
-# app = Dash(__name__, external_stylesheets=external_stylesheets)
+external_stylesheets = [dbc.themes.CERULEAN]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 # # df = pd.read_csv('coin_rankings.csv')
 # df = pd.read_csv('coin_rankings_fulldata.csv', parse_dates=['Date'])
